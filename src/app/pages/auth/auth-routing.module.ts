bee76ccbb3d63039728659cad39f0fdfb0210c9a
zeynepdenizgundogan <zeynep.deniz.gundogan@hotmail.com>
2024-12-07 18:50:51 +0300

    signup fnished login update

--- src/app/pages/auth/auth-routing.module.ts
+++ src/app/pages/auth/auth-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)//sdf
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)//sdf
   }
 
 ];
